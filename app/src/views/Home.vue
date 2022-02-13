acbee68cd3254b87d77237ecde4d35cc53812e07
bilal2536 <bilal.jellouli25@gmail.com>
2022-02-13 19:51:35 +0100

    Setup the project and make the search header [HTML/TAILWINDCSS/JS]

---
+++ app/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex flex-col h-screen max-h-screen">
+    <div class="z-20 flex justify-center relative bg-black bg-hero-pattern bg-cover px-4 pt-8 pb-32">
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'Home',
+  components: {}
+}
+</script>
