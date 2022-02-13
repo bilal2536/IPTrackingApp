a3df16c08ff7e74e4d4bb520478084538dc62bed
bilal2536 <bilal.jellouli25@gmail.com>
2022-02-13 20:03:50 +0100

    make the input search [HTML/TAILWINDCSS/JS]

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -1,6 +1,29 @@
 <template>
   <div class="flex flex-col h-screen max-h-screen">
     <div class="z-20 flex justify-center relative bg-black bg-hero-pattern bg-cover px-4 pt-8 pb-32">
+      <div class="w-full max-w-screen-sm">
+        <h1 class="text-white text-center text-3xl pb-4">IP Address Tracker</h1>
+        <div class="flex">
+          <input
+            v-model="queryIp"
+            class="flex-1 py-3 px-2 rounded-tl-md rounded-bl-md focus:outline-none"
+            type="text"
+            placeholder="Search for any IP address or leave empty to get your ip info"
+          />
+          <i
+            @click="getIpInfo"
+            class="cursor-pointer
+          bg-black
+          text-white
+          px-4
+          rounded-tr-md
+          rounded-br-md
+          flex
+          items-center
+          fas fa-chevron-right"
+          ></i>
+        </div>
+      </div>
     </div>
   </div>
 </template>
